51b881e54be51dceb10c41639444dc2684ce3384
Omar Nour <omar_nour@hotmail.com>
2017-08-21 12:51:55 +0300

    change DB connection

--- AccountingProject/settings.py
+++ AccountingProject/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'AccountingProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -101,7 +101,7 @@ DATABASES = {
         'HOST': 'omarNour.mysql.pythonanywhere-services.com',
     }
 }
-"""
+
 # 5432
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
